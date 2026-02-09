714d4c145b23dfb6273fbf1b64e2b1fa6d0d440b
Brian Kinney <bkinney0514@gmail.com>
2026-02-09 11:45:26 -0500

    OA Source

---
+++ ObstacleAssault/Source/ObstacleAssault/MovingPlatform.cpp
@@ -0,0 +1,83 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MovingPlatform.h"
+
+// Sets default values
+AMovingPlatform::AMovingPlatform()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+int MyTestFunction(float Param1, int Param2)
+{
+	int MyInt = Param2 * 2;
+	float Calc = Param1 / 3.5f;
+	UE_LOG(LogTemp, Display, TEXT("MyInt is %d and Calc is %f"), MyInt, Calc);
+
+	return 40;
+}
+
+// Called when the game starts or when spawned
+void AMovingPlatform::BeginPlay()
+{
+	Super::BeginPlay();
+
+	int ReturnValue = MyTestFunction(7.0f, 4);
+	UE_LOG(LogTemp, Display, TEXT("MyInt is %d"), ReturnValue);
+
+	StartLocation = GetActorLocation();
+}
+
+// Called every frame
+void AMovingPlatform::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	MovePlatform(DeltaTime);
+
+	RotatePlatform(DeltaTime);
+
+}
+
+float AMovingPlatform::GetDistanceMoved()
+{
+	return FVector::Dist(StartLocation, GetActorLocation());
+
+}
+
+void AMovingPlatform::MovePlatform(float DeltaTime)
+{
+	DistanceMoved = GetDistanceMoved();
+
+	if (DistanceMoved >= MoveDistance)
+	{
+		float Overshoot = DistanceMoved - MoveDistance;
+		FString PlatformName = GetName();
+		UE_LOG(LogTemp, Display, TEXT("%s overshot by %f"), *PlatformName, Overshoot);
+
+		FVector MoveDirection = PlatformVelocity.GetSafeNormal();
+		FVector NewStartLocation = StartLocation + MoveDirection * MoveDistance;
+		SetActorLocation(NewStartLocation);
+		StartLocation = NewStartLocation;
+
+		PlatformVelocity = -PlatformVelocity;
+	}
+	else
+	{
+		FVector CurrentLocation = GetActorLocation();
+
+		CurrentLocation = CurrentLocation + (PlatformVelocity * DeltaTime);
+
+		SetActorLocation(CurrentLocation);
+	}
+}
+
+void AMovingPlatform::RotatePlatform(float DeltaTime)
+{
+	FRotator RotationToAdd = RotationVelocity * DeltaTime;
+	AddActorLocalRotation(RotationToAdd);
+}
+
